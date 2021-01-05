134f3ba298e2e1c00546ab78a9ab72aae52bd30d
tranthuckhang2000 <73299797+tranthuckhang2000@users.noreply.github.com>
2021-01-05 08:57:49 +0700

    đổ dữ liệu cho iphone

---
+++ src/main/java/View/DataIphone.java
@@ -0,0 +1,17 @@
+package View;
+
+import Model.Iphone;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class DataIphone {
+    public static Map<String, Iphone> data = new HashMap<>();
+    static{
+     data.put("01", new Iphone("01", "Iphone 12 Pro Max", 8, 8,  "http://localhost:8080/Group_Project/images/iphone/iphone_12_pro_max/iphone-12-pro-max-den.jpg", "5",  "5",  "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5" ));
+        data.put("01", new Iphone("01", "Iphone 12 Pro Max", 8, 8,  "http://localhost:8080/Group_Project/images/iphone/iphone_12_pro_max/iphone-12-pro-max-den.jpg", "5",  "5",  "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5" ));
+        data.put("01", new Iphone("01", "Iphone 12 Pro Max", 8, 8,  "http://localhost:8080/Group_Project/images/iphone/iphone_12_pro_max/iphone-12-pro-max-den.jpg", "5",  "5",  "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5" ));
+
+    }
+}
