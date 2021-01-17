f60c09ee4057c0f0f22892bf19b7f21e80d2afc7
PhamThanhTu964809 <73974983+PhamThanhTu964809@users.noreply.github.com>
2021-01-17 17:04:14 +0700

    Revert "u"
    This reverts commit 6505a4c4c25e21dd61aa90970f61ba71e42f992d.

--- src/main/java/com/example/Group_Project/HelloServlet.java
+++ src/main/java/com/example/Group_Project/HelloServlet.java
@@ -1,10 +1,8 @@
 package com.example.Group_Project;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
