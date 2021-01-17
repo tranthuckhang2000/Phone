6505a4c4c25e21dd61aa90970f61ba71e42f992d
PhamThanhTu964809 <73974983+PhamThanhTu964809@users.noreply.github.com>
2021-01-17 17:02:31 +0700

    u

--- src/main/java/com/example/Group_Project/HelloServlet.java
+++ src/main/java/com/example/Group_Project/HelloServlet.java
@@ -1,8 +1,10 @@
 package com.example.Group_Project;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
